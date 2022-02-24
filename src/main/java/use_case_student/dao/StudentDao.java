b683ba846097b8eea52aa553a4f37e09048979b4
Nelson <nels.fomekong@gmail.com>
2022-02-24 12:25:34 +0100

    dossier student

---
+++ src/main/java/use_case_student/dao/StudentDao.java
@@ -0,0 +1,17 @@
+package use_case_student.dao;
+
+import use_case_student.entity.Student;
+
+import java.util.Collection;
+
+public interface StudentDao {
+    Collection<Student> getAllStudents();
+
+    Student getStudentById(int id);
+
+    void removeStudentById(int id);
+
+    void updateStudent(Student student);
+
+    void insertStudentToDb(Student student);
+}
